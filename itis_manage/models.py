151d8cb14e4154ac883992b95ca71c73b7e7b5aa
khaliullin <sgtkhaliullin@gmail.com>
2017-07-10 13:17:32 +0300

    Person model added

--- itis_manage/models.py
+++ itis_manage/models.py
@@ -1,4 +1,14 @@
 from django.db import models
 
+
 class Test(models.Model):
     about = models.TextField(help_text="Text")
+
+
+class Person(models.Model):
+    email = models.CharField(max_length=50, unique=True)
+    name = models.CharField(max_length=30)
+    surname = models.CharField(max_length=40)
+    third_name = models.CharField(max_length=40)
+    birth_date = models.CharField(max_length=40, null=True, blank=True)
+
