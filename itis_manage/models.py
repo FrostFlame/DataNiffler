ec33f71a2ba0ad852f289bf710ad8b6b5ec22666
Shevyakov <iashevyakov@mail.ru>
2017-07-10 14:21:36 +0300

    adding some models

--- itis_manage/models.py
+++ itis_manage/models.py
@@ -1,10 +1,6 @@
 from django.db import models
 
 
-class Test(models.Model):
-    about = models.TextField(help_text="Text")
-
-
 class Person(models.Model):
     email = models.CharField(max_length=50, unique=True)
     name = models.CharField(max_length=30)
@@ -12,3 +8,24 @@ class Person(models.Model):
     third_name = models.CharField(max_length=40)
     birth_date = models.CharField(max_length=40, null=True, blank=True)
 
+
+class Laboratory(models.Model):
+    name = models.CharField(max_length=40)
+    person = models.ForeignKey(Person, related_name='person_laboratories')
+
+
+class LaboratoryRequests(models.Model):
+    student = models.ForeignKey(Student, related_name='student_requests_for_labs')
+    laboratory = models.ForeignKey(Laboratory, related_name='laboratory_requests')
+    is_active = models.BooleanField(null=True)
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=10)
+
+
+class Student(models.Model):
+    status = models.CharField(max_length=1)
+    group = models.ForeignKey(Group, related_name='group_students')
+    person = models.OneToOneField(Person, related_name='person_student')
+    form_of_education = models.CharField(max_length=1)
