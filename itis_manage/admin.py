abbe204322465b0dc1bead03a3750a41608dfbc7
khaliullin <sgtkhaliullin@gmail.com>
2017-07-10 15:56:46 +0300

    Rest models added. Test model deleted.

--- itis_manage/admin.py
+++ itis_manage/admin.py
@@ -1,8 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-from itis_manage.models import Test
-
-@admin.register(Test)
-class TestAdmin(admin.ModelAdmin):
-    pass
--- itis_manage/models.py
+++ itis_manage/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 
-class Test(models.Model):
-    about = models.TextField(help_text="Text")
+class Status(models.Model):
+    name = models.CharField(max_length=30)
 
 
 class Person(models.Model):
@@ -11,4 +11,67 @@ class Person(models.Model):
     surname = models.CharField(max_length=40)
     third_name = models.CharField(max_length=40)
     birth_date = models.CharField(max_length=40, null=True, blank=True)
+    status = models.ManyToManyField(Status)
+
+
+class Subject(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Course(models.Model):
+    subject = models.OneToOneField(Subject)
+
+
+class Semester(models.Model):
+    semester = models.IntegerField(max_length=2)
+
+
+class SemesterSubject(models.Model):
+    EXAM = 'EX'
+    NOT_DIFF_TEST = "ND"
+    DIFF_TEST = "DT"
+
+    TYPE_OF_EXAM_CHOICES = (
+        (EXAM, 'Экзамен'),
+        (NOT_DIFF_TEST, 'Недифференцированный зачет'),
+        (DIFF_TEST, 'Дифференцированный зачет'),
+    )
+
+    subject = models.ForeignKey(Subject, related_name='subject_semesters')
+    semester = models.ForeignKey(Semester, related_name='semester_subjects')
+    type_of_exam = models.CharField(
+        max_length=2,
+        choices=TYPE_OF_EXAM_CHOICES,
+        default=EXAM
+    )
+
+    class Meta:
+        unique_together=(('semester', 'subject'),)
+
+
+class Progress(models.Model):
+    subject = models.ForeignKey(Subject)
+    # student = models.ForeignKey(Student)
+    semester = models.IntegerField(max_length=2)
+    practice = models.IntegerField(max_length=3)
+    exam = models.IntegerField(max_length=3)
+
+
+class TeacherSubject(models.Model):
+    PRACTICE = 'P'
+    LECTURE = 'L'
+    TYPE_CHOICES = (
+        (PRACTICE, 'Практика'),
+        (LECTURE, 'Лекция'),
+    )
+    type = models.CharField(max_length=1,
+                            choices=TYPE_CHOICES,
+                            default=LECTURE)
+
+    subject = models.ForeignKey(Subject)
+    person = models.ForeignKey(Person)
+    # group = models.ManyToManyField(Group)
+
+
+
 
