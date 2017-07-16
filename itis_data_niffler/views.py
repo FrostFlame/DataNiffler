d686ef7987c560338157cc8ffb6d9c9d704999e3
Shevyakov <iashevyakov@mail.ru>
2017-07-16 19:12:03 +0300

    #77 - group rating v0.3

--- itis_data_niffler/views.py
+++ itis_data_niffler/views.py
@@ -252,19 +252,21 @@ def group_rating(request):
                     else:
                         group_rating[group_score / progresses.count()].append(group)
                 group_score_by_subjects[group.id] = []
-                for subject in Subject.objects.all():
+                subjects = Subject.objects.filter(subject_semesters__semester__in=sems)
+                for subject in subjects:
                     group_subject_score = 0
-                    subject_progresses = Progress.objects.filter(semester_subject__semester__in=sems,
-                                                                 semester_subject__subject=subject,
+                    subject_progresses = Progress.objects.filter(semester_subject__subject=subject,
                                                                  student__in=group.group_students.all())
                     for p in subject_progresses:
                         group_subject_score += p.exam + p.practice
                     if subject_progresses.count() != 0:
                         group_subject_score = group_subject_score / subject_progresses.count()
                         group_score_by_subjects[group.id].append((group_subject_score, subject))
+                    else:
+                        group_score_by_subjects[group.id].append(('-', subject))
                     print(group_score_by_subjects)
             group_rating = sorted(group_rating.items(), key=lambda x: x[0], reverse=True)
         return render(request, 'itis_data_niffler/templates/group_rating.html',
                       {'form': make_form(form_name='form', form_init=init), 'group_rating': group_rating,
-                       'group_score_by_subjects': group_score_by_subjects})
+                       'group_score_by_subjects': group_score_by_subjects, 'subjects':subjects})
     return render(request, 'itis_data_niffler/templates/group_rating.html', ctx)
